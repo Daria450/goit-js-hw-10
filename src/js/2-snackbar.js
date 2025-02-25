f4ae12443e191175e59f3c8fc671e3f54a241a9e
Daria <shirai.ryu.hanzo.hasashi@gmail.com>
2025-02-25 21:47:02 +0200

    review 6

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -5,53 +5,52 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const refs = {
     form: document.querySelector(".form"),
-    fulfilledInp: document.querySelector(".fulfilled"),
-    rejectedInp: document.querySelector(".rejected"),
-    submitBtn: document.querySelector(".button"),
-    delayPr: document.querySelector(".form-number"),
 }
 
 
 const params = {
-    state: null,
-    delay: null,
+    state: refs.form.elements.state,
+    delay: refs.form.elements.state,
 }
+
+
 
 refs.form.addEventListener("submit", (e) => {
 
     e.preventDefault();
-    if (e.currentTarget.querySelector(".fulfilled").checked) {
-        params.state = "fulfilled";
+
+    function createPromise(delay, state) {
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (state === 'fulfilled') {
+                    resolve(delay);
+                } else {
+                    reject(delay);
+                }
+            }, delay);
+        })
+
     }
-    if (e.currentTarget.querySelector(".rejected").checked) {
-        params.state = "rejected";
-    }
-    params.delay = e.currentTarget.delay.value;
 
-    onSubmitBtnClick(refs.delayPr.value)
-});
+    const promise = createPromise(params.delay.value, params.state.value);
 
-function onSubmitBtnClick(delay) {
-
-    setTimeout(() => {
-        const promise = new Promise((resolve, reject) => {
-            if (params.state === "fulfilled") { resolve(`Fulfilled promise in ${delay}ms`); }
-            if (params.state === "rejected") { reject(`Rejected promise in ${delay}ms`); }
-        });
-        promise.then(value => {
+    promise
+        .then(delay => {
             iziToast.show({
                 title: '✅ OK',
-                message: value,
+                message: `Fulfilled promise in ${delay}ms`,
                 backgroundColor: '#59a10d',
                 messageColor: '#fff',
                 titleColor: '#fff',
                 position: 'topRight',
                 messageSize: '16px',
             })
-        }).catch(value => {
+        })
+        .catch(delay => {
             iziToast.show({
                 title: 'X Error',
-                message: value,
+                message: `Rejected promise in ${delay}ms`,
                 backgroundColor: '#ef4040',
                 messageColor: '#fff',
                 titleColor: '#fff',
@@ -59,8 +58,13 @@ function onSubmitBtnClick(delay) {
                 messageSize: '16px',
             })
         })
-        params.state = null;
-    }, params.delay)
-    params.delay = null;
-}
+
+    refs.form.reset();
+});
+
+
+
+
+
+
 
